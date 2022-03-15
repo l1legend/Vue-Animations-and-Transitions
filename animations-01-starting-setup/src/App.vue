12ccd9f72879a732eb8fbd2ddf48b2430c268e2f
Michael Min <michaelmin2020@gmail.com>
2022-03-15 17:31:08 -0500

    basic animation using css transform property

--- animations-01-starting-setup/src/App.vue
+++ animations-01-starting-setup/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <div class="block"></div>
-    <button>Animate</button>
+    <div class="block" :class="{animate: animatedBlock}"></div>
+    <button @click="animateBlock">Animate</button>
   </div>
   <base-modal @close="hideDialog" v-if="dialogIsVisible">
     <p>This is a test dialog!</p>
@@ -15,9 +15,15 @@
 <script>
 export default {
   data() {
-    return { dialogIsVisible: false };
+    return {
+      animatedBlock: false, 
+      dialogIsVisible: false,
+     };
   },
   methods: {
+    animateBlock() {
+      this.animatedBlock = true;
+    },
     showDialog() {
       this.dialogIsVisible = true;
     },
@@ -68,5 +74,8 @@ button:active {
   padding: 2rem;
   border: 2px solid #ccc;
   border-radius: 12px;
+}
+.animate {
+  transform: translateX(-50px);
 }
 </style>
